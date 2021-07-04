0025a7ea3342bd51a58cdbe2aa389a853a3c305a
Ben Gardner <1bengardner@gmail.com>
2021-07-04 10:54:28 -0400

    Add basic login page

---
+++ project/source/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>        
+    </head>
+    <body>
+        <form action="login-action.php" method="post">
+            <div>
+                <input type="email" name="email" placeholder="E-mail" maxlength="320" required />
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="Password" maxlength="255" required />
+            </div>
+            <div>
+                <input class="submit-button" type="submit" value="Log in" /></p>
+            </div>
+        </form>
+    </body>
+</html>

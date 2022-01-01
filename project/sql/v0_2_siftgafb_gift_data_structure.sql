7e2107548568635d898ccb06e04abca08ed0d488
Ben Gardner <1bengardner@gmail.com>
2022-01-01 03:23:10 -0500

    Add "active" attribute to gift

--- project/sql/v0_2_siftgafb_gift_data_structure.sql
+++ project/sql/v0_2_siftgafb_gift_data_structure.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2021 at 07:35 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.18
+-- Host: localhost:3306
+-- Generation Time: Nov 18, 2021 at 08:58 PM
+-- Server version: 10.3.31-MariaDB-log-cll-lve
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gift_data`
+-- Database: `siftgafb_gift_data`
 --
+CREATE DATABASE IF NOT EXISTS `siftgafb_gift_data` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `siftgafb_gift_data`;
 
 -- --------------------------------------------------------
 
@@ -27,8 +30,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `gift`
 --
 
+DROP TABLE IF EXISTS `gift`;
 CREATE TABLE IF NOT EXISTS `gift` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
   `name` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `notes` text DEFAULT NULL,
@@ -44,12 +49,23 @@ CREATE TABLE IF NOT EXISTS `gift` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique-user-email` (`email`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gift`
+--
+ALTER TABLE `gift`
+  ADD CONSTRAINT `fk-gift-user` FOREIGN KEY (`user`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

53a436d6ae02ca5aedce6daecc5d51672cdaafa7
Ben Gardner <1bengardner@gmail.com>
2022-02-18 03:57:55 -0500

    Insert reserved time

--- project/source/action/reserve-gift-toggle.php
+++ project/source/action/reserve-gift-toggle.php
@@ -5,9 +5,14 @@ require_once '../action/start-session.php';
 
 $gift = Gift::get_from_id($_POST["id"]);
 if ($gift->belongs_to_user($_SESSION["id"])) {
-  $stmt = "UPDATE gift SET reserved=? WHERE id=?";
-  Database::run_statement(Database::get_connection(), $stmt, [1 - $gift->reserved, $_POST["id"]]);
+  $reserved = 1 - $gift->reserved;
+  if ($reserved) {
+    $stmt = "UPDATE gift SET reserved=?, reserved_time=CURRENT_TIMESTAMP WHERE id=?";
+  } else {
+    $stmt = "UPDATE gift SET reserved=?, reserved_time=NULL WHERE id=?";    
+  }
+  Database::run_statement(Database::get_connection(), $stmt, [$reserved, $_POST["id"]]);
   header("HTTP/1.1 200 OK");
-  echo 1 - $gift->reserved; // new reservation status
+  echo $reserved; // new reservation status
 }
 ?>
--- project/source/action/reserve-gift.php
+++ project/source/action/reserve-gift.php
@@ -3,6 +3,6 @@ require_once '../util/utilities.php';
 
 // TODO: Validate fields
 
-$stmt = "UPDATE gift SET reserved=1 WHERE id=?";
+$stmt = "UPDATE gift SET reserved=1, reserved_time=CURRENT_TIMESTAMP WHERE id=?";
 Database::run_statement(Database::get_connection(), $stmt, [$_POST["id"]]);
 ?>

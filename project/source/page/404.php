e25a69eebc09a4b48f3ee40b1d263f22913259a4
Ben Gardner <1bengardner@gmail.com>
2022-02-28 20:55:18 -0500

    Add 404 page

---
+++ project/source/page/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <?php define('TITLE', '404'); ?>
+  <?php include 'head.php'; ?>
+  <body class="center">
+    <h1 class="logo-text">Sift<span class="accent"><span class="spaced">.</span>gifts</span></h1>
+    <svg viewBox="0 0 145 145" width="5em" height="5em" xmlns="http://www.w3.org/2000/svg"><ellipse cx="571.139" cy="596.292" rx="6.76" ry="21.534" transform="matrix(0.428529, -0.903528, 0.903528, 0.428529, -758.267273, 321.295074)" fill="#361f5c"/><ellipse cx="665.455" cy="623.753" rx="6.76" ry="21.534" transform="matrix(0.766735, -0.641963, 0.641963, 0.766735, -791.09021, 37.188538)" fill="#361f5c"/><circle cx="73.757" cy="80.767" r="43.067" fill="#361f5c"/><rect x="78.902" y="114.566" width="13.084" height="23.442" fill="#361f5c"/><rect x="52.734" y="114.566" width="13.084" height="23.442" fill="#361f5c"/><ellipse cx="81.082" cy="138.281" rx="10.903" ry="4.089" fill="#361f5c"/><ellipse cx="54.915" cy="137.736" rx="10.903" ry="4.089" fill="#361f5c"/><path d="M 60.234 26.63 C 64.08 11.143 81.054 2.029 98.149 6.274 C 115.243 10.518 125.983 26.514 122.137 42.001 C 118.292 57.488 105.533 57.538 88.439 53.293 C 71.345 49.049 56.388 42.117 60.234 26.63 Z" fill="#d5b3d9"/><circle cx="67.318" cy="71.679" r="14.359" fill="#fff"/><circle cx="61.417" cy="66.532" r="4.786" fill="#28225A"/><path d="M 76.326 101.595 C 77.645 108.844 70.622 114.804 63.684 112.322 C 60.464 111.17 58.103 108.388 57.49 105.023 L 57.49 105.023 L 57.487 105.005 C 56.545 99.803 60.567 97.962 65.769 97.02 C 70.972 96.078 75.384 96.393 76.326 101.595 Z" fill="#fff"/></svg>
+    <p></p>
+    <h2 class="subheading">Page not found!</h2>
+    <h3 class="subheading">Sorry, there's nothing located at this link.</h3>
+    <p><a href="/">Return</a></p>
+  </body>
+</html>

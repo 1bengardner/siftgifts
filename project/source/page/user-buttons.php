d6224823ec14ff9e85db98647285e3c6bec6af35
Ben Gardner <1bengardner@gmail.com>
2022-03-12 00:10:40 -0500

    feat: improve header appearance

--- project/source/page/user-buttons.php
+++ project/source/page/user-buttons.php
@@ -2,24 +2,24 @@
 require_once '../data/user.php';
 require_once '../action/start-session.php';
 ?>
-<ul class="user-buttons center">
-  <div class="user-header-buttons">
+<div class="user-buttons center">
+  <ul class="user-header-buttons">
     <li class="purple-box username-box"><a class="link" href="../page/home"><?php echo ucwords(strtolower(User::get_from_id($_SESSION['id'])->username)) ?></a></li>
     <li><a href="/page/settings" title="Change settings">👤</a></li>
     <li><a href="/page/logout" title="Log out">🚪</a></li>
-  </div>
+  </ul>
   <div>
-    <span class="user-header-buttons-large">
+    <ul class="user-header-buttons-large">
       <li class="purple-box username-box"><a class="link" href="../page/home"><?php echo ucwords(strtolower(User::get_from_id($_SESSION['id'])->username)) ?></a></li>
-    </span>
-    <span class="wishlist-header-buttons">
-      <li><a class="link" href="/page/wishlist">Wishlist</a></li>
+    </ul>
+    <ul class="wishlist-header-buttons">
+      <li><a class="wishlist-header-label link" href="/page/wishlist">Wishlist</a></li>
       <li><a href="/page/wishlist" title="Edit wishlist">📝</a></li>
       <li><a href="/page/request" title="Add to wishlist">➕</a></li>
-    </span>
-    <span class="user-header-buttons-large">
+    </ul>
+    <ul class="user-header-buttons-large">
       <li><a href="/page/settings" title="Change settings">👤</a></li>
       <li><a href="/page/logout" title="Log out">🚪</a></li>
-    </span>
+    </ul>
   </div>
-</ul>
+</div>

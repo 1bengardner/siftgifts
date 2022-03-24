ef67860ed031e814b7afbb97da6a84a6db8b264b
Ben Gardner <1bengardner@gmail.com>
2022-03-23 23:05:47 -0400

    feat: use UserMessage object

--- project/source/action/show-messages.php
+++ project/source/action/show-messages.php
@@ -18,12 +18,12 @@ require_once '../action/authenticate.php';
     <?php
     } else {
       foreach ($msgs as $msg_data) {
-        $msg = ($msg_data);
+        $msg = new UserMessage($msg_data);
     ?>
         <div class="message-chooser-message">
           <div class="right">
             <?php
-            $date = strtotime($msg['sent_time']);
+            $date = strtotime($msg->sent_time);
             $date_format = 'g:i A';
             if ($date < strtotime('today')) {
               $date_format = 'M j';
@@ -32,11 +32,11 @@ require_once '../action/authenticate.php';
               $date_format .= ', Y';
             }
             ?>
-            <span><?php echo date($date_format, strtotime($msg['sent_time'])); ?></span>
+            <span><?php echo date($date_format, strtotime($msg->sent_time)); ?></span>
           </div>
           <div>
-            <p><strong><?php echo ucwords(strtolower(User::get_from_id($msg['from'])->username)); ?></strong></p>
-            <p class="preview subheading"><?php echo $msg['body']; ?></p>
+            <p><strong><?php echo ucwords(strtolower(User::get_from_id($msg->from)->username)); ?></strong></p>
+            <p class="preview subheading"><?php echo $msg->body; ?></p>
           </div>
         </div>
       <?php

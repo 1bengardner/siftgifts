5505a1e014028fc72aa422f676656188e9a59a93
Ben Gardner <1bengardner@gmail.com>
2022-04-05 13:51:02 -0400

    refactor: message content

--- project/source/page/js/messaging.js
+++ project/source/page/js/messaging.js
@@ -1,21 +1,25 @@
-function setContent(id) {
+function setContent(content) {
+  document.querySelectorAll('.message-content')[0].innerHTML = "<p>" + content + "</p>";  
+}
+
+function setMessage(id) {
   var rq = new XMLHttpRequest();
   rq.open("GET", "/action/get-message?id=" + id, true);
   rq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   rq.onreadystatechange = function() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       clearTimeout(loading);
-      document.querySelectorAll('.message-content')[0].innerHTML = "<p>" + rq.responseText + "</p>";
+      setContent(rq.responseText);
     }
   }
   var loading = setTimeout(function() {
-    document.querySelectorAll('.message-content')[0].innerHTML = "<p>" + "Loading…" + "</p>";
+    setContent("Loading…");
   }, 500);
   rq.send();
 }
 
 document.querySelectorAll('.message-chooser-message').forEach(message => {
   message.onclick = function() {
-    setContent(message.id);
+    setMessage(message.id);
   }
 });

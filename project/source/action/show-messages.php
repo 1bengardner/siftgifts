bb8461a133728fd770b971479363d918337ea2bf
Ben Gardner <1bengardner@gmail.com>
2022-04-06 19:59:35 -0400

    feat: include sent messages in previews

--- project/source/action/show-messages.php
+++ project/source/action/show-messages.php
@@ -3,8 +3,8 @@ require_once '../util/utilities.php';
 require_once '../data/user-message.php';
 require_once '../action/authenticate.php';
 
-$stmt = "SELECT * FROM message m1 JOIN (SELECT `from`, MAX(sent_time) AS most_recent FROM message WHERE `to`=? GROUP BY `from`) m2 ON m1.`from` = m2.`from` and m1.sent_time = m2.most_recent ORDER BY sent_time DESC";
-$res = Database::run_statement(Database::get_connection(), $stmt, [$_SESSION['id']]);
+$stmt = "SELECT * FROM (SELECT * FROM message m1 JOIN (SELECT `from` conversation_partner, MAX(sent_time) most_recent FROM message WHERE `to`=? GROUP BY `from`) m2 ON m1.`from` = m2.conversation_partner and m1.sent_time = m2.most_recent UNION (SELECT * FROM message m1 JOIN (SELECT `to`, MAX(sent_time) most_recent FROM message WHERE `from`=? GROUP BY `to`) m2 ON m1.`to` = m2.`to` and m1.sent_time = m2.most_recent)) res GROUP BY conversation_partner ORDER BY sent_time DESC";
+$res = Database::run_statement(Database::get_connection(), $stmt, [$_SESSION['id'], $_SESSION['id']]);
 $msgs = $res->fetch_all(MYSQLI_ASSOC);
 if (count($msgs) === 0) {
 ?>
@@ -19,6 +19,7 @@ if (count($msgs) === 0) {
 <?php
   foreach ($msgs as $msg_data) {
     $msg = new UserMessage($msg_data);
+    $msg->conversation_partner = $msg_data['conversation_partner']
 ?>
     <div class="message-chooser-message" id=<?php echo $msg->id; ?>>
       <div class="right">
@@ -35,8 +36,16 @@ if (count($msgs) === 0) {
         <span><?php echo date($date_format, strtotime($msg->sent_time)); ?></span>
       </div>
       <div>
-        <p><strong><?php echo ucwords(strtolower(User::get_from_id($msg->from)->username)); ?></strong></p>
-        <p class="preview subheading"><?php echo htmlentities($msg->body); ?></p>
+        <p><strong><?php echo ucwords(strtolower(User::get_from_id($msg->conversation_partner)->username)); ?></strong></p>
+        <p class="preview subheading">
+          <?php
+          if ($msg->from === $_SESSION['id']) {
+            echo '<em>'.htmlentities($msg->body).'</em>';
+          } else {
+            echo htmlentities($msg->body);
+          }
+          ?>
+        </p>
       </div>
     </div>
 <?php

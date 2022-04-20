a3610b7153a8cdfd43c5ca72ffe563d28cde8e0a
Ben Gardner <1bengardner@gmail.com>
2022-04-20 19:40:10 -0400

    feat: improve conversation window layout

--- project/source/action/show-messages.php
+++ project/source/action/show-messages.php
@@ -35,32 +35,32 @@ if (count($msgs) === 0) {
       class="message-chooser-message <?php if ($msg->unread && ($msg->from != $_SESSION['id'] || $msg->conversation_partner_id == $_SESSION['id'])) echo "unread"; ?>"
       conversation="<?php echo $msg->conversation_partner_id; ?>"
       last-message="<?php echo $msg->id; ?>">
-      <div class="right">
-      <?php
-      $date = strtotime($msg->sent_time);
-      $date_format = 'g:i A';
-      if ($date < strtotime('first day of january this year')) {
-        $date_format = 'Y';
-      } else if ($date < strtotime('last Sunday')) {
-        $date_format = 'M j';
-      } else if ($date < strtotime('today')) {
-        $date_format = 'l';
-      }
-      ?>
-        <span class="last-message-time"><?php echo date($date_format, strtotime($msg->sent_time)); ?></span>
-      </div>
       <div>
-        <p><strong>
-        <?php
-        if (isset($msg->conversation_partner_id)) {
-          echo ucwords(strtolower(User::get_from_id($msg->conversation_partner_id)->username));
-        } else if (isset($msg->guest_name)) {
-          echo '<em>❓</em> '.ucwords(strtolower($msg->guest_name));
-        } else {
-          echo '<em>👻</em>';
-        }
-        ?>
-        </strong></p>
+        <p class="preview">
+          <strong>
+          <?php
+          if (isset($msg->conversation_partner_id)) {
+            echo ucwords(strtolower(User::get_from_id($msg->conversation_partner_id)->username));
+          } else if (isset($msg->guest_name)) {
+            echo '<em>❓</em> '.ucwords(strtolower($msg->guest_name));
+          } else {
+            echo '<em>👻</em>';
+          }
+          ?>
+          </strong>
+          <?php
+          $date = strtotime($msg->sent_time);
+          $date_format = 'g:i A';
+          if ($date < strtotime('first day of january this year')) {
+            $date_format = 'Y';
+          } else if ($date < strtotime('last Sunday')) {
+            $date_format = 'M j';
+          } else if ($date < strtotime('today')) {
+            $date_format = 'l';
+          }
+          ?>
+          <span class="preview right last-message-time"><?php echo date($date_format, strtotime($msg->sent_time)); ?></span>
+        </p>
         <p class="preview subheading">
           <?php
           if ($msg->from === $_SESSION['id']) {

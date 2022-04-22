da9df05ac4356ef940e48409325ffb53f65bd32b
Ben Gardner <1bengardner@gmail.com>
2022-04-22 00:41:13 -0400

    fix: unread message has lighter background on date

--- project/source/page/js/messaging.js
+++ project/source/page/js/messaging.js
@@ -19,7 +19,7 @@ function setContent(replyable, ...messageData) {
   messageData.forEach(function(msg) {
     let sentDate = new Date(msg['sent_time']);
     let sentToday = !(sentDate.getDate() < currentDate.getDate() || sentDate.getMonth() < currentDate.getMonth() || sentDate.getYear() < currentDate.getYear())
-  content += `<div class='${msg['is_sender'] ? 'sent-message' : 'received-message'}'><p${msg['unread'] ? " class='unread'" : ""}><span ${!msg['unread'] && msg['is_sender'] ? "title='Seen'" : ""} class='right message-time-sent'>${sentToday ? "" : "<span class='muted "+(msg['unread'] ? 'unread' : '')+"'>"+new Date(msg['sent_time']).toLocaleString(undefined, localeStrings['date'])+"</span> "}${new Date(msg['sent_time']).toLocaleString(undefined, localeStrings['time'])}</span>${linkify(msg['message'])}</p></div>`;
+  content += `<div class='${msg['is_sender'] ? 'sent-message' : 'received-message'}'><p${msg['unread'] ? " class='unread'" : ""}><span ${!msg['unread'] && msg['is_sender'] ? "title='Seen'" : ""} class='right message-time-sent'>${sentToday ? "" : "<span class='muted'>"+new Date(msg['sent_time']).toLocaleString(undefined, localeStrings['date'])+"</span> "}${new Date(msg['sent_time']).toLocaleString(undefined, localeStrings['time'])}</span>${linkify(msg['message'])}</p></div>`;
   });
   document.querySelectorAll('.message-content')[0].classList.remove('old-content');
   document.querySelectorAll('.message-content')[0].innerHTML = content;

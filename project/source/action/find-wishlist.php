eab3d0cb623f51d542247f0fbbcd276f2a434d96
Ben Gardner <1bengardner@gmail.com>
2022-04-26 22:42:06 -0400

    fix: lowercase wishlist links

--- project/source/action/find-wishlist.php
+++ project/source/action/find-wishlist.php
@@ -5,7 +5,7 @@ require_once 'start-session.php';
 
 if (isset($_GET['u']) && $_GET['u']) {
   if (($user = User::get_from_name(trim($_GET['u']))) && isset($user->username)) {
-    header('Location: /sg/'.$user->username);
+    header('Location: /sg/'.strtolower($user->username));
   } else {
     $_SESSION["notifications"] = [new Notification(NotificationText::WishlistNotFound, NotificationLevel::Info)];
   }

3b371abea8deb9152c997ac9caaa67c44e996029
Ben Gardner <1bengardner@gmail.com>
2022-05-01 11:23:20 -0400

    feat: improve 403 return button text

--- project/source/page/403.php
+++ project/source/page/403.php
@@ -7,6 +7,6 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="65" height="120" viewBox="700 300 200 200" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="795.07343" cy="405.2348" r="79" fill="#361f5c"/><rect x="970.03671" y="568.90295" width="24" height="43" transform="translate(1801.11014 1079.13775) rotate(-180)" fill="#361f5c"/><rect x="922.03671" y="568.90295" width="24" height="43" transform="translate(1705.11014 1079.13775) rotate(-180)" fill="#361f5c"/><ellipse cx="811.07343" cy="510.7348" rx="20" ry="7.5" fill="#361f5c"/><ellipse cx="763.07343" cy="509.7348" rx="20" ry="7.5" fill="#361f5c"/><circle cx="793.07343" cy="385.2348" r="27" fill="#fff"/><circle cx="793.07343" cy="385.2348" r="9" fill="#28225A"/><path d="M1034.66938,433.43522c6.37889-28.56758-14.01185-57.43391-45.544-64.47476s-62.2651,10.41-68.644,38.97759S935,447.04185,966.53215,454.0827,1028.29049,462.0028,1034.66938,433.43522Z" transform="translate(-162.96329 -141.66814)" fill="#ffeb63"/></svg>
     <h2 class="subheading">Oh no!</h2>
     <h3 class="subheading">You aren't supposed to be here.</h3>
-    <p><a href="/">Return</a></p>
+    <p><a href="/">Return to Sift.gifts</a></p>
   </body>
 </html>

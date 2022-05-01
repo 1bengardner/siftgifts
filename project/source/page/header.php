5458c35d9d8f9c51f697825d0cce1e75ec0920c6
Ben Gardner <1bengardner@gmail.com>
2022-04-30 23:56:45 -0400

    feat: add signup and login to header on user pages

--- project/source/page/header.php
+++ project/source/page/header.php
@@ -1,13 +1,11 @@
+<?php
+require_once '../action/start-session.php';
+if (isset($_SESSION["id"])) {
+?>
 <nav class="header">
   <div class="nav-grid">
     <h1 class="logo-text unbreakable"><a class="link" href="/" title="Home">Sift<span class="accent"><span class="spaced">.</span>gifts</span></a></h1>
-    <?php
-    require_once '../action/start-session.php';
-
-    if (isset($_SESSION["id"])) {
-      include 'user-buttons.php';
-    }
-    ?>
-    </span>
+    <?php include 'user-buttons.php'; ?>
   </div>
 </nav>
+<?php } else { include 'header-with-login.php'; } ?>

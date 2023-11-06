8f86da436e424ae1582773ea79fcd2f596be21ab
Ben Gardner <1bengardner@gmail.com>
2023-11-05 19:38:06 -0500

    fix: remove nonfunctional-on-000webhost stuff

---
+++ project/source/page/forgot-password-temp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <?php define('TITLE', 'Forgot password'); ?>
+  <?php include 'head.php'; ?>
+  <body>
+    <?php include '../action/redirect-to-home.php'; ?>
+    <form action="/action/submit-forgot-password.php" method="post">
+      <h1 class="logo-text">Sift<span class="accent"><span class="spaced">.</span>gifts</span></h1>
+      <h2>Forgot your password?</h2>
+      <?php include 'notification-box.php'; ?>
+      <div>
+        <p>Sorry to hear&hellip;let Ben know and he will sort that out for you.</p>
+      </div>
+      <div class="links-section">
+        <a class="link" href="/login">⬅️ Go back to login</a>
+      </div>
+    </form>
+  </body>
+  <script src="/page/js/extra-flavour.js" type="text/javascript"></script>
+</html>

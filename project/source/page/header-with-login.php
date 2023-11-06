0399791c61fc90d149ab787fdbbc11ac9d1efe0d
Ben Gardner <1bengardner@gmail.com>
2023-11-05 21:32:46 -0500

    feat: add landing image

--- project/source/page/header-with-login.php
+++ project/source/page/header-with-login.php
@@ -1,6 +1,6 @@
 <nav class="header">
   <div class="nav-grid">
-    <h1 class="logo-text unbreakable"><a class="link" href="/" title="Main page">Sift<span class="accent"><span class="spaced">.</span>gifts</span></a></h1>
+    <h1 class="logo-text unbreakable"><a class="link" href="/" title="Main page"><img src="/page/img/present.png" width="32px" alt="" />Sift<span class="accent"><span class="spaced">.</span>gifts</span></a></h1>
     <ul class="right">
       <li>
         <h2><a href="/login">Log in</a></h2>

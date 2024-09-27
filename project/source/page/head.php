24055e9a79f206dde1e01310d424930449c1d633
Ben Gardner <1bengardner@gmail.com>
2024-09-27 19:16:01 -0400

    uncache css

--- project/source/page/head.php
+++ project/source/page/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/page/css/style.css?v=4" />
+    <link rel="stylesheet" type="text/css" href="/page/css/style.css?v=5" />
     <link rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Fuggles&text=Sift.gifts!?">
     <link rel="stylesheet"

83333592a4b279647207fe2b077190b88a0d6ac3
Ben Gardner <1bengardner@gmail.com>
2024-12-21 12:11:36 -0500

    fix: prizes

--- project/source/page/lottery.php
+++ project/source/page/lottery.php
@@ -62,24 +62,7 @@ if (!in_array($user->id, [1, 2])) {
         }
         ?>
       </div>
-      <div>
-        <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes</summary>
-          <div>
-            <p>Seven numbers will be drawn.</p><p class="subheading">If one or more of yours match, you win!</p>
-          </div>
-          <div style="display: inline-block;">
-          <ul style="margin: 0; text-align: left;">
-            <li><strong>1/7</strong>: Free play</li>
-            <li><strong>2/7</strong>: A Lotto Max ticket</li>
-            <li><strong>3/7</strong>: A steak dinner</li>
-            <li><strong>4/7</strong>: Anything from your wishlist</li>
-            <li><strong>5/7</strong>: $500</li>
-            <li><strong>6/7</strong>: $5,000</li>
-            <li><strong>7/7</strong>: A baby</li>
-          </ul>
-          </div>
-        </details>
-      </div>
+      <?php include 'prizes.php' ?>
       <hr>
       <div>
         <a href="/home">🏠 Return home</a>
---
+++ project/source/page/prizes.php
@@ -0,0 +1,18 @@
+<div>
+  <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes</summary>
+    <div>
+      <p>Seven numbers will be drawn.</p><p class="subheading">If one or more of yours match, you win!</p>
+    </div>
+    <div style="display: inline-block;">
+    <ul style="margin: 0; text-align: left;">
+      <li><strong>1/7</strong>: Free play</li>
+      <li><strong>2/7</strong>: A Lotto Max ticket</li>
+      <li><strong>3/7</strong>: A steak dinner</li>
+      <li><strong>4/7</strong>: Anything from your wishlist</li>
+      <li><strong>5/7</strong>: $500</li>
+      <li><strong>6/7</strong>: $5,000</li>
+      <li><strong>7/7</strong>: A puppy</li>
+    </ul>
+    </div>
+  </details>
+</div>

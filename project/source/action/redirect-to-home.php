1366a5f2fa4e578b9d09847af9a1328b8e479759
Ben Gardner <1bengardner@gmail.com>
2024-12-21 02:12:07 -0500

    feat: lottery

--- project/source/action/redirect-to-home.php
+++ project/source/action/redirect-to-home.php
@@ -2,6 +2,12 @@
 require_once 'start-session.php';
 
 if (isset($_SESSION["id"])) {
+    require_once '../data/user.php';
+    $user = User::get_from_id($_SESSION['id']);
+    if (in_array($user->id, [1, 2])) {
+      header("Location: /lottery");
+      exit;
+    }
     header("Location: /home");
     exit;
 }

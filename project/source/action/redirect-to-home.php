9709ade6727909d7ec9f15499a4c938b5db4616d
Ben Gardner <1bengardner@gmail.com>
2024-12-22 00:46:07 -0500

    feat: add explicit lottery access

--- project/source/action/redirect-to-home.php
+++ project/source/action/redirect-to-home.php
@@ -4,10 +4,6 @@ require_once 'start-session.php';
 if (isset($_SESSION["id"])) {
     require_once '../data/user.php';
     $user = User::get_from_id($_SESSION['id']);
-    if (in_array($user->id, [1, 2])) {
-      header("Location: /lottery");
-      exit;
-    }
     header("Location: /home");
     exit;
 }

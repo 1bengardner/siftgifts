d6e33b2c7733d3d544c16037c8bca086697017ac
Ben Gardner <1bengardner@gmail.com>
2024-12-27 18:42:42 -0500

    feat: new prize notice

--- project/source/page/prizes.php
+++ project/source/page/prizes.php
@@ -1,5 +1,5 @@
 <div>
-  <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes</summary>
+  <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes 🆕</summary>
     <div>
       <p>Seven numbers will be drawn.</p><p class="subheading">If one or more of yours match, you win!</p>
     </div>
@@ -7,7 +7,7 @@
     <ul style="margin: 0; text-align: left;">
       <li><strong>1/7</strong>: Free play</li>
       <li><strong>2/7</strong>: A Lotto Max ticket</li>
-      <li><strong>3/7</strong>: Balloons</li>
+      <li><strong>3/7</strong> 🆕: Balloons</li>
       <li><strong>4/7</strong>: Anything from your wishlist</li>
       <li><strong>5/7</strong>: $500</li>
       <li><strong>6/7</strong>: $5,000</li>

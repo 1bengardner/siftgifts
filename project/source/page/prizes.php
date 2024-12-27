de986172acfd652147fd5da0e6838215c3a698ac
Ben Gardner <1bengardner@gmail.com>
2024-12-27 18:36:09 -0500

    feat: update prizes

--- project/source/page/prizes.php
+++ project/source/page/prizes.php
@@ -7,7 +7,7 @@
     <ul style="margin: 0; text-align: left;">
       <li><strong>1/7</strong>: Free play</li>
       <li><strong>2/7</strong>: A Lotto Max ticket</li>
-      <li><strong>3/7</strong>: A steak dinner</li>
+      <li><strong>3/7</strong>: Balloons</li>
       <li><strong>4/7</strong>: Anything from your wishlist</li>
       <li><strong>5/7</strong>: $500</li>
       <li><strong>6/7</strong>: $5,000</li>

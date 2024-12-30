e471f0770b7d750d1e6bff4528e6dc564e3a1f12
Ben Gardner <1bengardner@gmail.com>
2024-12-29 23:07:00 -0500

    feat: show blanks for winning numbers if ticket dne

--- project/source/action/get-winning-ticket.php
+++ project/source/action/get-winning-ticket.php
@@ -4,6 +4,9 @@ require_once 'authenticate.php';
 
 $stmt = "CALL get_winning_ticket(?)";
 $res = Database::run_statement(Database::get_connection(), $stmt, [$_SESSION['id']])->fetch_all(MYSQLI_NUM);
+if (!$res) {
+  return ["🚫", "🚫", "🚫", "🚫", "🚫", "🚫", "🚫"];
+}
 $ticket_numbers = $res[0];
 array_shift($ticket_numbers); // strip ID
 array_pop($ticket_numbers); // strip draw time

38710847397f414014ee43d24cedb615c9c0a793
Ben Gardner <1bengardner@gmail.com>
2025-04-10 23:32:25 -0400

    feat: remove special NYE case

--- project/source/page/prizes.php
+++ project/source/page/prizes.php
@@ -9,27 +9,6 @@ if (!$res) {
   echo "<p class='widget' style='display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;'>Cannot view prizes. You are not enrolled in a lottery.</p>";
   exit;
 }
-// Special case for NYE draw
-if (array_shift($res[0]) == 6) {
-?>
-  <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes 🎊</summary>
-    <div>
-      <p>Seven numbers will be drawn.</p><p class="subheading">If one or more of yours match, you win!</p>
-    </div>
-    <div style="display: inline-block;">
-    <ul style="margin: 0; text-align: left;">
-      <li><strong>1/7</strong>: Free play</li>
-      <li><strong>2/7</strong>: Good luck throughout 2025</li>
-      <li><strong>3/7</strong>: Anything from your wishlist</li>
-      <li><strong>4/7</strong>: $100</li>
-      <li><strong>5/7</strong>: $500</li>
-      <li><strong>6/7</strong>: $9,000</li>
-      <li><strong>7/7</strong>: A guppy</li>
-    </ul>
-    </div>
-  </details>
-<?php
-} else {
 ?>
   <details class="widget" style="display: inline-block; padding: 1em; margin: 0 0 1em 0; border-radius: 2em;"><summary style="cursor: pointer;">Prizes 🆕</summary>
     <div>
@@ -53,7 +32,4 @@ if (array_shift($res[0]) == 6) {
     </ul>
     </div>
   </details>
-<?php
-}
-?>
 </div>

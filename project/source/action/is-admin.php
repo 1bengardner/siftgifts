03b5915a7f3c797bb22c0a9d5c3e4ba867e2d89b
Ben Gardner <1bengardner@gmail.com>
2025-05-09 00:57:17 -0400

    fix: variable scope bug

--- project/source/action/is-admin.php
+++ project/source/action/is-admin.php
@@ -2,6 +2,5 @@
 require_once 'authenticate.php';
 require_once '../data/user.php';
 
-$user = User::get_from_id($_SESSION['id']);
-return in_array($user->id, [2]);
+return in_array(User::get_from_id($_SESSION['id'])->id, [2]);
 ?>

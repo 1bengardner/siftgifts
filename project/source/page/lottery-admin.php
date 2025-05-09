8097298c22727f27436a2e59db417250a2c1b5c8
Ben Gardner <1bengardner@gmail.com>
2025-05-08 23:57:30 -0400

    feat: prize admin

--- project/source/page/lottery-admin.php
+++ project/source/page/lottery-admin.php
@@ -7,19 +7,52 @@ if (!in_array($user->id, [2])) {
   header("Location: /home");
   exit;
 }
+require_once '../util/utilities.php';
+$stmt = "CALL get_prizes(?)";
+$prizes = Database::run_statement(Database::get_connection(), $stmt, [$_SESSION['id']])->fetch_all(MYSQLI_ASSOC);
+$prizes = $prizes ? $prizes[0] : $prizes = array_fill(1, 7, "prize missing from db");
 ?>
 <!DOCTYPE html>
 <html>
-  <?php define('TITLE', 'Sift.gifts - Lottery Administration'); ?>
+  <?php define('TITLE', 'Lottery administration'); ?>
   <?php include 'head.php'; ?>
   <body>
-    <?php include 'header.php'; ?>    
+    <?php include 'header.php'; ?>
     <form action="/action/submit-regenerate-lottery-numbers.php" method="post">
+    <?php include 'notification-box.php'; ?>
       <div class="widget settings-widget">
         <h2 class="first-in-series connected-text">Lottery Administration</h2>
-        <?php include 'notification-box.php'; ?>
         <div>
           <input class="submit-button" type="submit" value="🌀 Generate new numbers for pending lotteries" />
+        </div>
+      </div>
+    </form>
+    <form action="/action/submit-change-prizes.php" method="post">
+      <div class="widget settings-widget">
+        <h2 class="first-in-series">Prize Administration</h2>
+        <div>
+          <input name="1" placeholder="Prize 1" value="<?php echo htmlentities($prizes[1]); ?>" required />
+        </div>
+        <div>
+          <input name="2" placeholder="Prize 2" value="<?php echo htmlentities($prizes[2]); ?>" required />
+        </div>
+        <div>
+          <input name="3" placeholder="Prize 3" value="<?php echo htmlentities($prizes[3]); ?>" required />
+        </div>
+        <div>
+          <input name="4" placeholder="Prize 4" value="<?php echo htmlentities($prizes[4]); ?>" required />
+        </div>
+        <div>
+          <input name="5" placeholder="Prize 5" value="<?php echo htmlentities($prizes[5]); ?>" required />
+        </div>
+        <div>
+          <input name="6" placeholder="Prize 6" value="<?php echo htmlentities($prizes[6]); ?>" required />
+        </div>
+        <div>
+          <input name="7" placeholder="Prize 7" value="<?php echo htmlentities($prizes[7]); ?>" required />
+        </div>
+        <div>
+          <input class="submit-button" type="submit" value="🏆 Update prizes" />
         </div>
       </div>
       <div>

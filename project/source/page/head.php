5a61251960cd23d2d82ca9444d636cbe5d44561b
Ben Gardner <1bengardner@gmail.com>
2025-12-10 12:11:31 -0500

    Add og:image

--- project/source/page/head.php
+++ project/source/page/head.php
@@ -1,6 +1,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
+    <meta property="og:image" content="https://sift.gifts/assets/logo.png">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="/page/css/style.css?v=15" />
     <link rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Fuggles&text=Sift.gifts!?">

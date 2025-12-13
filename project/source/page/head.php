b352eb2e68b4f0bcf9b40f9f66a40b3b4de5b554
Ben Gardner <1bengardner@gmail.com>
2025-12-13 13:35:45 -0500

    Bust cache

--- project/source/page/head.php
+++ project/source/page/head.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta property="og:image" content="https://sift.gifts/assets/logo.png">
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" type="text/css" href="/page/css/style.css?v=16" />
+    <link rel="stylesheet" type="text/css" href="/page/css/style.css?v=17" />
     <link rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Fuggles&text=Sift.gifts!?">
     <link rel="stylesheet"

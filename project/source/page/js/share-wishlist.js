efaf820e40efefcfcf801f66398000355b3397a0
Ben Gardner <1bengardner@gmail.com>
2026-03-02 12:16:14 -0500

    Fix share button private wishlist link

--- project/source/page/js/share-wishlist.js
+++ project/source/page/js/share-wishlist.js
@@ -1,11 +1,9 @@
-let wishlistUrl = document.querySelectorAll('.clipboard-button')[0].getAttribute('url');
-
 if (navigator.canShare) {
   document.querySelectorAll('.clipboard-button').forEach(function(elem) {
     elem.title = "Share";
     elem.onclick = () => navigator.share({
       title: 'Sift.gifts wishlist link',
-      url: wishlistUrl
+      url: elem.getAttribute('url');
     });
     elem.innerHTML = "<strong>Share!</strong>";
   });

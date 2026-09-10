a1460e75adc751ae48680577b21f3ea713ef0f75
Ben Gardner <1bengardner@gmail.com>
2026-09-09 21:46:44 -0400

    Fit wishlist name in table column

--- project/source/action/submit-wishlist-name-change.php
+++ project/source/action/submit-wishlist-name-change.php
@@ -5,7 +5,7 @@ require_once 'authenticate.php';
 $short_name = $_POST['id'];
 
 $stmt = "UPDATE wishlist SET name = ? WHERE short_name = ? AND owner = ?";
-Database::run_statement(Database::get_connection(), $stmt, [$_POST['name'], $short_name, $_SESSION['id']]);
+Database::run_statement(Database::get_connection(), $stmt, [mb_substr($_POST['name'], 0, 36), $short_name, $_SESSION['id']]);
 
 $stmt = "SELECT name FROM wishlist WHERE short_name = ? AND owner = ?";
 $new_name = Database::run_statement(Database::get_connection(), $stmt, [$short_name, $_SESSION['id']])->fetch_row()[0];

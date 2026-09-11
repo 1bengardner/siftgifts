36cac8d1c3701a3858d27a9cf2d34270a60591a6
Ben Gardner <1bengardner@gmail.com>
2026-09-11 14:42:30 -0400

    Fix unreserve 500
    - Worked fine locally; TODO check config

--- project/source/action/record-gift-action.php
+++ project/source/action/record-gift-action.php
@@ -5,7 +5,7 @@ function recordGiftReservation($what, $got_reserved, $who) {
   $stmt = "INSERT INTO gift_audit(gift, reserve, user, connecting_ip, forwarding_ips) VALUES (?, ?, ?, ?, ?)";
   Database::run_statement(Database::get_connection(), $stmt, [
     $what,
-    $got_reserved,
+    $got_reserved ? 1 : 0,
     $who,
     empty($_SERVER['REMOTE_ADDR']) ? null : $_SERVER['REMOTE_ADDR'],
     empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? null : $_SERVER['HTTP_X_FORWARDED_FOR'],
